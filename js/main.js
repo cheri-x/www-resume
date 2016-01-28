1fba554a0a5524be7abf352e1781a177e079edc6
cheri-x <buchnowska.anna@gmail.com>
2016-01-28 21:01:11 +0100

    modifications

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$('.barra-nivel').each(function() 
-{
-  var valorLargura = $(this).data('nivel');
-  var valorNivel = $(this).html("<span class='valor-nivel'>"+valorLargura+"</span>");
-    $(this).animate({
-        width: valorLargura
-    });
-});
